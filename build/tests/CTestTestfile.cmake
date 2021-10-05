c2622d7b8f44e15a4bee5c7156071e079cd6bbea
Berthe Nsiemeni <nsiemeni1u@etu.univ-lorraine.fr>
2021-10-05 14:56:18 +0200

    first organized c make

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,36 @@
+# CMake generated Testfile for 
+# Source directory: O:/FirstOrganizedCMake/tests
+# Build directory: O:/FirstOrganizedCMake/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+if("${CTEST_CONFIGURATION_TYPE}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+  add_test(test_hello "O:/FirstOrganizedCMake/build/tests/Debug/test_hello.exe")
+  set_tests_properties(test_hello PROPERTIES  _BACKTRACE_TRIPLES "O:/FirstOrganizedCMake/tests/CMakeLists.txt;11;add_test;O:/FirstOrganizedCMake/tests/CMakeLists.txt;0;")
+elseif("${CTEST_CONFIGURATION_TYPE}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+  add_test(test_hello "O:/FirstOrganizedCMake/build/tests/Release/test_hello.exe")
+  set_tests_properties(test_hello PROPERTIES  _BACKTRACE_TRIPLES "O:/FirstOrganizedCMake/tests/CMakeLists.txt;11;add_test;O:/FirstOrganizedCMake/tests/CMakeLists.txt;0;")
+elseif("${CTEST_CONFIGURATION_TYPE}" MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+  add_test(test_hello "O:/FirstOrganizedCMake/build/tests/MinSizeRel/test_hello.exe")
+  set_tests_properties(test_hello PROPERTIES  _BACKTRACE_TRIPLES "O:/FirstOrganizedCMake/tests/CMakeLists.txt;11;add_test;O:/FirstOrganizedCMake/tests/CMakeLists.txt;0;")
+elseif("${CTEST_CONFIGURATION_TYPE}" MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+  add_test(test_hello "O:/FirstOrganizedCMake/build/tests/RelWithDebInfo/test_hello.exe")
+  set_tests_properties(test_hello PROPERTIES  _BACKTRACE_TRIPLES "O:/FirstOrganizedCMake/tests/CMakeLists.txt;11;add_test;O:/FirstOrganizedCMake/tests/CMakeLists.txt;0;")
+else()
+  add_test(test_hello NOT_AVAILABLE)
+endif()
+if("${CTEST_CONFIGURATION_TYPE}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+  add_test(test_version "O:/FirstOrganizedCMake/build/tests/Debug/test_version.exe")
+  set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "O:/FirstOrganizedCMake/tests/CMakeLists.txt;15;add_test;O:/FirstOrganizedCMake/tests/CMakeLists.txt;0;")
+elseif("${CTEST_CONFIGURATION_TYPE}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+  add_test(test_version "O:/FirstOrganizedCMake/build/tests/Release/test_version.exe")
+  set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "O:/FirstOrganizedCMake/tests/CMakeLists.txt;15;add_test;O:/FirstOrganizedCMake/tests/CMakeLists.txt;0;")
+elseif("${CTEST_CONFIGURATION_TYPE}" MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+  add_test(test_version "O:/FirstOrganizedCMake/build/tests/MinSizeRel/test_version.exe")
+  set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "O:/FirstOrganizedCMake/tests/CMakeLists.txt;15;add_test;O:/FirstOrganizedCMake/tests/CMakeLists.txt;0;")
+elseif("${CTEST_CONFIGURATION_TYPE}" MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+  add_test(test_version "O:/FirstOrganizedCMake/build/tests/RelWithDebInfo/test_version.exe")
+  set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "O:/FirstOrganizedCMake/tests/CMakeLists.txt;15;add_test;O:/FirstOrganizedCMake/tests/CMakeLists.txt;0;")
+else()
+  add_test(test_version NOT_AVAILABLE)
+endif()
--- src/log_lib/print_log.cpp
+++ src/log_lib/print_log.cpp
@@ -1,13 +1,11 @@
-
-
 #include <iostream>
-#include<FirstOrganizedCMakeConfig.h>
+#include< FirstOrganizedCMakeConfig.h >
 #include<log_lib/print_log.h>
 
 void hello_world() {
-	std::cout << "Hello World!\n" << std::endl;
+std::cout << "Hello World!\n" << std::endl;
 }
 
 void print_version() {
-	std::cout << "project version is "	<< FirstOrganizedCMake_VERSION_MAJOR << "." << FirstOrganizedCMake_VERSION_MINOR << std::endl;
+std::cout << "project version is " << FirstCMake_VERSION_MAJOR << "." << FirstCMake_VERSION_MINOR << std::endl;
 }
